7ba0e056e26d2468aad547d01398d9bd753700ad
reeechart <ferdinandusrichard@yahoo.co.id>
2018-11-08 17:18:06 +0700

    add weight/coefs initialization after fitting

--- classifier/feed_forward_neural_network.py
+++ classifier/feed_forward_neural_network.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class FeedForwardNeuralNetwork:
     def __init__(self, learning_rate, hidden_layer_sizes, tol=0.0001, batch_size=1, momentum=0):
         self.learning_rate = learning_rate
@@ -7,21 +9,33 @@ class FeedForwardNeuralNetwork:
         self.momentum = momentum
         self._check_validity()
         self.coefs_ = []
-
-    def _initialize_coefs(self):
-        print('initializing coefs...')
-
-    def _initialize_intercepts(self):
-        print('initializing intercepts...')
+        self.intercepts_ = []
 
     def _check_validity(self):
-        if (not self._hidden_layer_sizes_valid()):
+        if (not self._is_hidden_layer_sizes_valid()):
             raise(AttributeError('Hidden layer sizes %s is invalid' % (self.hidden_layer_sizes,)))
     
-    def _hidden_layer_sizes_valid(self):
+    def _is_hidden_layer_sizes_valid(self):
         _valid_length = (len(self.hidden_layer_sizes) in range (0, 11))
         _valid_content = all(x > 0 for x in self.hidden_layer_sizes)
         return _valid_length and _valid_content
+
+    def _initialize_coefs(self, data):
+        input_neuron_size = len(data[0])
+        output_neuron_size = 1
+        neuron_sizes = [input_neuron_size] + self.hidden_layer_sizes + [output_neuron_size]
+        for neuron_index in range (len(neuron_sizes)-1):
+            layer_weight = []
+            for _ in range(neuron_sizes[neuron_index]):
+                neuron_weight = []
+                for _ in range(neuron_sizes[neuron_index+1]):
+                    neuron_weight.append(0)
+                layer_weight.append(neuron_weight)
+            layer_weight = np.array(layer_weight)
+            self.coefs_.append(layer_weight)
+
+    def _initialize_intercepts(self):
+        print('initializing intercepts...')
     
     def _feed_forward_phase(self, data):
         return None
@@ -33,4 +47,5 @@ class FeedForwardNeuralNetwork:
         return None
 
     def fit(self, data):
+        self._initialize_coefs(data)
         print('fitting...')
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from classifier.feed_forward_neural_network import FeedForwardNeuralNetwork
 
-clf_ffnn = FeedForwardNeuralNetwork(learning_rate=0.4, hidden_layer_sizes=(7,6,5,4,4,3,2,1,1,1))
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+clf_ffnn = FeedForwardNeuralNetwork(learning_rate=0.4, hidden_layer_sizes=[7,6,5,4,4,3,2,1,1,1])
 print(clf_ffnn.learning_rate)
 print(clf_ffnn.hidden_layer_sizes)
 print(clf_ffnn.tol)
 print(clf_ffnn.batch_size)
 print(clf_ffnn.momentum)
+
+clf_ffnn.fit(iris.data)
+print(clf_ffnn.coefs_)
