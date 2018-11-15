a824a45f008220edfc5e1c3d9ff6f7f187586cdd
reeechart <ferdinandusrichard@yahoo.co.id>
2018-11-15 14:19:15 +0700

    able to update weights

--- classifier/feed_forward_neural_network.py
+++ classifier/feed_forward_neural_network.py
@@ -16,6 +16,8 @@ class FeedForwardNeuralNetwork:
         self.intercepts_ = []
         self.gradients_ = []
         self.delta_gradients_ = []
+        self.delta_coefs = []
+        self.layers_input = []
         self.error_ = 0
         self.result_ = 0
 
@@ -41,6 +43,7 @@ class FeedForwardNeuralNetwork:
                 layer_weight.append(neuron_weight)
             layer_weight = np.array(layer_weight)
             self.coefs_.append(layer_weight)
+            self.delta_coefs.append(layer_weight)
 
     def _initialize_intercepts(self, data):
         output_neuron_size = 1
@@ -52,7 +55,8 @@ class FeedForwardNeuralNetwork:
             bias_layer = np.array(bias_layer)
             self.intercepts_.append(bias_layer)
     
-    def _reset_gradients(self, data):
+    def _initialize_gradients(self):
+        ''' Reset the gradients to initial weight '''
         self.gradients_ = []
         output_neuron_size = 1
         neuron_gradient_sizes = self.hidden_layer_sizes + [output_neuron_size]
@@ -63,27 +67,38 @@ class FeedForwardNeuralNetwork:
             self.gradients_.append(np.array(layer_gradient))
 
     def _compress_delta_gradient(self):
+        compressed_delta_gradients = []
         for index in range(len(self.delta_gradients_)):
-            self.delta_gradients_[index] = np.sum(self.delta_gradients_[index], axis=0)
+            compressed_delta_gradients.append(np.sum(self.delta_gradients_[index], axis=0))
+
+        return compressed_delta_gradients
     
     def _feed_forward_phase(self, batch_data, batch_target):
+        self.layers_input = []
+        self.delta_gradients_ = []
+        self.layers_input.append(np.array(batch_data))
         self.result_ = np.array(batch_data)
         for layer_index in range(len(self.coefs_)):
             self.result_ = np.matmul(self.result_, self.coefs_[layer_index])
-
-            # save to delta_gradients
-            self.delta_gradients_.append(self.result_)
 
             # add with bias and convert all results with sigmoid function
             for result_index in range(len(self.result_)):
                 self.result_[result_index] = [x+y for x, y in zip(self.result_[result_index], self.intercepts_[layer_index])]
                 self.result_[result_index] = logistic.cdf(self.result_[result_index])
 
-        print('this is delta gradient with length of %d' % len(self.delta_gradients_))
-        print(self.delta_gradients_)
+            # save to delta_gradients
+            self.delta_gradients_.append(deepcopy(self.result_))
+            if (layer_index != (len(self.coefs_)-1)):
+                self.layers_input.append(deepcopy(self.result_))
 
-        # delta_gradient = self._compress_delta_gradient(delta_gradient)
-        # self.gradients_ += delta_gradient
+        # print('this is delta gradient with length of %d' % len(self.delta_gradients_))
+        # print(self.delta_gradients_)
+
+        print('this is layers input with length of %d' % len(self.layers_input))
+        print(self.layers_input)
+
+        # print('this is the result')
+        # print(self.result_)
 
         diff = self.result_.ravel() - batch_target
         self.error_ += np.sum(0.5 * diff * diff)
@@ -99,6 +114,10 @@ class FeedForwardNeuralNetwork:
                 self.delta_gradients_[-1] = self.delta_gradients_[-1] * (1 - self.delta_gradients_[-1]) * np.reshape(diff, (len(diff), 1))
             else:
                 # hidden layers
+                print('this is coefs')
+                print(self.coefs_)
+                print('this is delta_gradients')
+                print(self.delta_gradients_)
                 sum_outputs_gradients = np.matmul(self.delta_gradients_[dg_length-i], np.transpose(self.coefs_[dg_length-i]))
                 self.delta_gradients_[dg_length-i-1] = self.delta_gradients_[dg_length-i-1] * (1 - self.delta_gradients_[dg_length-i-1]) * sum_outputs_gradients
             
@@ -106,7 +125,18 @@ class FeedForwardNeuralNetwork:
         print(self.delta_gradients_)
 
     def _update_weight(self, batch_data, batch_target):
-        pass
+        compressed_delta_input = []
+        for i in range(len(self.delta_gradients_)):
+            compressed_delta_input.append(np.matmul(np.transpose(self.layers_input[i]), self.delta_gradients_[i]))
+        print('this is compressed delta input')
+        print(compressed_delta_input)
+        
+        print('this is delta coefs')
+        print(self.delta_coefs)
+        compressed_delta_input = np.array(compressed_delta_input)
+        for i in range(len(self.delta_coefs)):
+            self.delta_coefs[i] = self.learning_rate * compressed_delta_input[i] + self.momentum * self.delta_coefs[i]
+            self.coefs_[i] = np.add(self.coefs_[i], self.delta_coefs[i])
 
     def _split_data(self, data, target):
         batch_data_list = []
@@ -127,22 +157,20 @@ class FeedForwardNeuralNetwork:
     def fit(self, data, target):
         self._initialize_coefs(data)
         self._initialize_intercepts(data)
+        self._initialize_gradients()
         batch_data_list, batch_target_list = self._split_data(data, target)
         batches = math.ceil(len(data)/self.batch_size)
-        print(batch_data_list)
-        print(batch_target_list)
 
         # epochs started
-        # iter = 0
-        # while (iter < self.max_iter and self.error_ > self.tol):
-        self.error_ = 0
-        for batch_index in range(batches):
-            self._reset_gradients(batch_data_list[batch_index])
-            res = self._feed_forward_phase(batch_data_list[batch_index], batch_target_list[batch_index])
-            print("this is the result for batch %d" % batch_index)
-            print(res)
-            print("doing backpropagation...")
-            print("==================================================================")
-            self._backpropagation(batch_target_list[batch_index], res)
-            self._update_weight(batch_data_list[batch_index], batch_target_list[batch_index])
-            # iter += 1
+        iter = 0
+        while ((iter < self.max_iter and self.error_ > self.tol) or (iter == 0)):
+            self.error_ = 0
+            for batch_index in range(batches):
+                res = self._feed_forward_phase(batch_data_list[batch_index], batch_target_list[batch_index])
+                print("this is the result for batch %d" % batch_index)
+                print(res)
+                print("doing backpropagation...")
+                print("==================================================================")
+                self._backpropagation(batch_target_list[batch_index], res)
+                self._update_weight(batch_data_list[batch_index], batch_target_list[batch_index])
+            iter += 1
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn import datasets
 
 iris = datasets.load_iris()
 
-clf_ffnn = FeedForwardNeuralNetwork(learning_rate=0.4, hidden_layer_sizes=[5,2], batch_size=len(iris.data))
+clf_ffnn = FeedForwardNeuralNetwork(learning_rate=0.4, hidden_layer_sizes=[5,2], batch_size=len(iris.data), max_iter=4)
 print(clf_ffnn.learning_rate)
 print(clf_ffnn.hidden_layer_sizes)
 print(clf_ffnn.tol)
@@ -12,7 +12,7 @@ print(clf_ffnn.batch_size)
 print(clf_ffnn.momentum)
 
 clf_ffnn.fit(iris.data, iris.target)
+print('last coef')
 print(clf_ffnn.coefs_)
+print('last intercepts')
 print(clf_ffnn.intercepts_)
-print('this is gradient')
-print(clf_ffnn.gradients_)
